2ad20cb71f0d3f98db538d7ec54ef22e3b5a7be3
Dominic Austria <da.624514@gmail.com>
2021-02-21 11:14:48 -0500

    test git token

--- encoding/hex_encode.py
+++ encoding/hex_encode.py
@@ -51,7 +51,6 @@ if __name__ == "__main__":
     # print(hex_to_byte("deadbeef123456"))
 
     print(hex_to_bytes("63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d"))
-    # >>>
     # crypto{You_will_be_working_with_hex_strings_a_lot}
 
 
