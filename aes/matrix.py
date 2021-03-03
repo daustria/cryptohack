3e010b84a47bbc255a2ff3b7a4970635bb184686
Dominic Austria <da.624514@gmail.com>
2021-03-03 16:56:20 -0500

    complete the initial matrix challenge for aes

---
+++ aes/matrix.py
@@ -0,0 +1,38 @@
+
+def bytes2matrix(text):
+    """ Converts a 16-byte array into a 4x4 matrix.  """
+    return [list(text[i:i+4]) for i in range(0, len(text), 4)]
+
+def matrix2bytes(matrix):
+    plaintext_bytes = ""
+
+    for row in matrix:
+        # assuming each how has 4 bytes.. eg something like [19, 121, 225, 57]
+        for i in range(0, 4):
+            plaintext_bytes = plaintext_bytes + chr(row[i])
+
+    return plaintext_bytes
+
+if __name__ == "__main__":
+
+    # this is the matrix which i have to decrypt..
+    # the flag encrypts the plaintext crypto{inmatrix}
+    matrix = [
+        [99, 114, 121, 112],
+        [116, 111, 123, 105],
+        [110, 109, 97, 116],
+        [114, 105, 120, 125],
+    ]
+
+
+
+    print(bytes2matrix("sixteencharacter"))
+
+    # the string deadbeef in hex is repeated 4 times, making for 16 bytes
+    # (two hex strings convert to one byte)
+    deadbeef_bytes = bytes.fromhex("deadbeefdeadbeefdeadbeefdeadbeef")
+
+    print(bytes2matrix(deadbeef_bytes))
+    
+    
+    print(matrix2bytes(matrix))
