8352562863e980cf64029859d1a73574244a18df
Dominic Austria <da.624514@gmail.com>
2021-03-11 18:01:25 -0500

    work on the passwords as keys challenge

--- aes/block_cipher_modes/passwords_as_keys.py
+++ aes/block_cipher_modes/passwords_as_keys.py
@@ -5,28 +5,41 @@ def get_flag():
     r = requests.get('http://aes.cryptohack.org/passwords_as_keys/encrypt_flag/')
     return r.json()['ciphertext']
 
+def decrypt(ciphertext, password_hash):
+    r = requests.get('http://aes.cryptohack.org/passwords_as_keys/decrypt/' + ciphertext + '/' + password_hash)
+    return r.json()['plaintext']
+
 if __name__ == '__main__':
 
     # the user is MD5 hashing a random word from word.txt
-    # the length is small enough to simply hash all the words myself and check for matching ciphertexts..
+    # the length of words.txt is small enough for a brute force attack
 
-
-    with open("words.txt") as f:
-        words = [w.strip() for w in f.readlines()]
-    passwords = [hashlib.md5(w.encode()).digest() for w in words]
+    #edit: nevermind, brute forcing takes too long...
 
     flag = get_flag()
-    # flag starts with 'crypto{' and ends with '}'
-    flag = flag[14:-2]
 
     print("flag: {}".format(flag))
-    for w in words:
 
-        # after i narrow down possible passwords, im hoping i can do some non-automated work to
-        # get the result...
-        password_hash = hashlib.md5(w.encode()).digest().hex()
-        print(password_hash)
+    with open("words.txt") as f:
 
-        if flag[:len(password_hash)] == password_hash:
-            print("Possible password: {} , Hash : {}".format(w, password_hash))
+        while(1):
+
+            word = f.readline()
+
+            if word == '':
+                break
+
+            word = word.strip()
+
+            possible_key = hashlib.md5(word.encode()).digest().hex()
+
+            plaintext = decrypt(flag, possible_key)
+
+            plaintext = bytes.fromhex(plaintext)
+
+            print(plaintext)
+
+            if plaintext[:6] == b'crypto':
+                print("possible key word : {}".format(word))
+                print("possible password_hash : {}".format(possible_key))
 
